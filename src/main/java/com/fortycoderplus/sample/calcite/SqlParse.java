171c1ec2608ce2f1696544691bdf676334f8b2de
caozf <caozhifu@chalco-steering.com>
2023-01-26 15:01:43 +0800

    init project

--- src/main/java/com/fortycoderplus/sample/calcite/SqlParse.java
+++ src/main/java/com/fortycoderplus/sample/calcite/SqlParse.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2017-2023 40CoderPlus. All rights reserved.
+ * (c) Copyright 2023 40CoderPlus. All rights reserved.
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,15 +18,21 @@
  * limitations under the License.
  */
 
-package com.fortycoderplus.template;
+package com.fortycoderplus.sample.calcite;
 
-import java.util.stream.Stream;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.dialect.PostgresqlSqlDialect;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.util.SqlString;
 
-public class Main {
+public class SqlParse {
 
-    public static void main(String[] args) {
-        Stream.of("Gradle Single Module Template".split(" "))
-                .map(word -> word.repeat(3))
-                .forEach(System.out::println);
+    public static void main(String[] args) throws SqlParseException {
+        SqlParser parser = SqlParser.create(
+                "SELECT * FROM t1 ORDER BY ID LIMIT 10", SqlParser.Config.DEFAULT);
+        SqlNode sqlNode = parser.parseStmt();
+        SqlString sqlString = sqlNode.toSqlString(PostgresqlSqlDialect.DEFAULT);
+        System.out.println(sqlString.getSql());
     }
 }
